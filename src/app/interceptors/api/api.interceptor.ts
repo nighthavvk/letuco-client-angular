7f638fcb58c7168523e1c79457e58679e7fb5966
Maksym <maxcharuk@gmail.com>
2020-05-30 13:53:45 +0300

    Add GH pages deployment proccess. Add env variables

--- src/app/interceptors/api/api.interceptor.ts
+++ src/app/interceptors/api/api.interceptor.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req.clone({ url: `http://localhost:3000${req.url}` }));
+    return next.handle(req.clone({ url: `${environment.apiUrl}${req.url}` }));
   }
 }
